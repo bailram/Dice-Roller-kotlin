42aef3d37169939b2c6459451f8eef4a6daecdec
bailram <dhimasbayu.9f@gmail.com>
2020-09-16 14:38:45 +0700

    Add OnClickListener and Change button text

--- app/src/main/java/com/bailram/dice_roller_kotlin/MainActivity.kt
+++ app/src/main/java/com/bailram/dice_roller_kotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.bailram.dice_roller_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
