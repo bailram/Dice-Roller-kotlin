1fd29b344071c646ddedc823304f6b18f38c7c63
bailram <dhimasbayu.9f@gmail.com>
2020-09-16 12:38:35 +0700

    Making first project

---
+++ app/src/main/java/com/bailram/dice_roller_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bailram.dice_roller_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
