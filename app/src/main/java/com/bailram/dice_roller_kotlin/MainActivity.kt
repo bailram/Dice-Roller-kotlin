960daaa4456273247208ee751a62b3983963c593
bailram <dhimasbayu.9f@gmail.com>
2020-09-18 09:16:15 +0700

    Adding the imageView Dice

--- app/src/main/java/com/bailram/dice_roller_kotlin/MainActivity.kt
+++ app/src/main/java/com/bailram/dice_roller_kotlin/MainActivity.kt
@@ -3,7 +3,10 @@ package com.bailram.dice_roller_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,25 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val randomInt = Random.nextInt(6)+1
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+//        diceImage.setImageDrawable(getDrawable(drawableResource)) // nggae iki yo iso
+        diceImage.setImageResource(drawableResource)
     }
 }
